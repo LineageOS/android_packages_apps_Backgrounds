861e1e0f8e211339a20f5fc8d61b94d2eae6fe53
althafvly <althafvly@gmail.com>
2022-04-16 11:10:53 +0530

    Backgrounds: use old wallpapers optionally
    Change-Id: I3a4f340399d074ee0720dd9bdd7445bd406b1739

--- Android.mk
+++ Android.mk
@@ -20,9 +20,19 @@ include $(CLEAR_VARS)
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
 
 ifeq ($(shell test $(TARGET_SCREEN_WIDTH) -gt 1080; echo $$?),0)
-LOCAL_RESOURCE_DIR += $(LOCAL_PATH)/res_1440p
+LOCAL_RESOURCE_DIR += $(LOCAL_PATH)/res_1440p/common
+ifeq ($(TARGET_INCLUDE_OLD_WALLPAPERS), true)
+LOCAL_RESOURCE_DIR += $(LOCAL_PATH)/res_1440p/full
 else
-LOCAL_RESOURCE_DIR += $(LOCAL_PATH)/res_1080p
+LOCAL_RESOURCE_DIR += $(LOCAL_PATH)/res_1440p/small
+endif
+else
+LOCAL_RESOURCE_DIR += $(LOCAL_PATH)/res_1080p/common
+ifeq ($(TARGET_INCLUDE_OLD_WALLPAPERS), true)
+LOCAL_RESOURCE_DIR += $(LOCAL_PATH)/res_1080p/full
+else
+LOCAL_RESOURCE_DIR += $(LOCAL_PATH)/res_1080p/small
+endif
 endif
 
 LOCAL_USE_AAPT2 := true
--- generate_smallvariants.py
+++ generate_smallvariants.py
@@ -5,8 +5,10 @@ from PIL import Image
 
 path = os.path.dirname(os.path.realpath(__file__))
 
-resources = ["res_1080p/drawable-nodpi",
-             "res_1440p/drawable-nodpi"]
+resources = ["res_1080p/common/drawable-nodpi",
+             "res_1080p/full/drawable-nodpi",
+             "res_1440p/common/drawable-nodpi",
+             "res_1440p/full/drawable-nodpi"]
 
 def generate_smallvariants(resource):
     global path
